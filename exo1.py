d98a0a2cefce4e12174e8a4e60fe3ba4ba9b2c61
Nadir Belarouci <nadir.belarrouci@gmail.com>
2018-02-06 15:45:46 +0100

    implement cote parcours -> not finished need to fix bugs

--- exo1.py
+++ exo1.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import sys
+from operator import itemgetter
 
 def lectureFichier(s): # Definition d'une fonction, avec un parametre (s). Ne pas oublier les ":"
 	monFichier = open(s, "r"); # Ouverture en lecture. Indentation par rapport a la ligne d'avant (<-> bloc).
@@ -13,7 +15,7 @@ def lectureFichier(s): # Definition d'une fonction, avec un parametre (s). Ne pa
 def openPref(nomFichier,nb_ligne_a_suppr):
 	contenu=lectureFichier(nomFichier);
 	cap = None;
-	if nomFichier == "TestPrefSpe.txt":
+	if nomFichier == sys.argv[2]:
 		cap = contenu[1][1:];
 	contenu = contenu[nb_ligne_a_suppr:]; #Supprimer les premières ligne des fichier (NbEtu 11)
 	leng = len(contenu);
@@ -47,7 +49,34 @@ def prefer(result, res, master_pref, prefEtu):
 			sub=couple_master[i]; # Permet de sauvegarder le couple avant de l'ecraser
 			couple_master[i]=res; # Remplace le couple moins interressant par le nouveau
 			res=sub; # Le couple moins interressant devra être interroger à nouveau si il reste de la place dans le master
-	prefEtu[res[1]]=(prefEtu[res[1]][0], False); # Le couple le moins interressant sera remi à False (car il est plus assigné avec un master)
+	prefEtu[res[1]]=(prefEtu[res[1]][0], False); # L'etudiant le moins interressant sera remi à False (car il est plus assigné avec un master)
+	for i in range(len(couple_master)): # On va remettre tous les couples les plus interressant dans result
+		prefEtu[int(couple_master[i][1])]=(prefEtu[int(couple_master[i][1])][0], True); # On met a True tous les etudiants figurant dans couple_master
+		result.append(couple_master[i]);
+	# print(result, res);
+	return result, prefEtu; # On retourne le tableau result et les preferences des etudiants qui ont etaient modifie
+	
+# Fonction qui va regarder si le master préfère le nouvel etudiant (res = tuple(master, student))
+# Par rapport a ses preferences (master_pref)
+# Et modifira par la meme occasion le tuple prefEtu (array pref, booleen si il a un master) 
+def prefer_cote_parcours(result, res, student_pref, prefEtu):
+	couple_master=[]; #tableau de tuple de tous les couples d'un meme master
+	i=0; # iteration dans la tableau result
+	while i < (len(result)): # Tant que i n'atteint pas la fin du tableau
+		# print(result[i], (res[0]))
+		if int(result[i][0])==int(res[0]): # Si le couple inclu la master en question
+			couple_master.append(result.pop(i)); # On ajoute ce couple dans la tableau des couples du meme master
+			# On le retire par la même occasion du tableau result
+			i=i-1; #On decremente i car on vient d'enlever un element du tableau
+		i=i+1;
+	# print(len(couple_master));
+	for i in range(len(couple_master)): # Pour tous les couples avec le meme master
+		# Si l'index du nouveau couple a inserer est plus petit (donc le master prefere le nouveau)
+		if student_pref[0].index(int(couple_master[i][0]))>student_pref[0].index(int(res[0])):
+			sub=couple_master[i]; # Permet de sauvegarder le couple avant de l'ecraser
+			couple_master[i]=res; # Remplace le couple moins interressant par le nouveau
+			res=sub; # Le couple moins interressant devra être interroger à nouveau si il reste de la place dans le master
+	prefEtu[res[1]]=(prefEtu[res[1]][0], False); # L'etudiant le moins interressant sera remi à False (car il est plus assigné avec un master)
 	for i in range(len(couple_master)): # On va remettre tous les couples les plus interressant dans result
 		prefEtu[int(couple_master[i][1])]=(prefEtu[int(couple_master[i][1])][0], True); # On met a True tous les etudiants figurant dans couple_master
 		result.append(couple_master[i]);
@@ -63,7 +92,7 @@ def masters_contains_false(preference_masters):
 			return True;
 	return False; # Sinon on retourne false
 	
-# Fonction qui va appliquer l'algorithme de gale shapley
+# Fonction qui va appliquer l'algorithme de gale shapley cote etudiant
 def gale_shapley_impl(prefSpe,prefEtu):
 	i=0; 
 	result = []; # this will hold tuple of our result (Student,Master)
@@ -102,12 +131,55 @@ def gale_shapley_impl(prefSpe,prefEtu):
 	# print(result);
 	return result;
 
+# Fonction qui va appliquer l'algorithme de gale shapley cote parcours
+def gale_shapley_parcours(prefSpe,prefEtu):
+	i=0; 
+	result = []; # this will hold tuple of our result (Student,Master)
+	lenEtu = len(prefEtu);
+	cap = prefSpe[1]; # get the capacity of each branch in a list 1D
+	prefSpe = prefSpe[0]; # get the branches matrix 2D 
+	while masters_contains_false(prefSpe): # Tant qu'il y a un master qui aura de la place
+		if(i>=len(prefSpe)): # Si la valeur i dépasse l'indice max du tableau des parcours
+			i=0; # on reini i
+		while(prefSpe[i][1]==True): # On va passer les parcours qui sont plein
+			i=i+1; # On incrément i pour passer à l'etudiant suivant
+			if(i>=len(prefSpe)): # On controle si on ne depasse pas 
+				i=0;
+		courantSpe = prefSpe[i]; # On va prendre les preference du parcours qui n'est pas plein
+		while prefSpe[i][1]==False: # Tant qu'on lui a pas assigner un etudiant
+			# On enleve l'etudiant prefere du master
+			master_wish = courantSpe[0].pop(0); # On recupere l'etudiant prefere
+			res = (int(master_wish), i); # On forme un tuple (id Etudiant, id Master)
+			# print(res);
+			if prefEtu[int(master_wish)][1]==False: # Si il reste de la place dans le master
+				result.append(res); # On ajoute le tuple dans le tableau de resultat
+				# On ajoute un etudiant au nb d'etudiant du master
+				addStudent=(prefSpe[i][0], prefSpe[i][1] ,prefSpe[i][2]+1);
+				prefSpe[i]=addStudent;
+				# Si le master à atteint la capacite max
+				if prefSpe[i][2]==int(cap[i]):
+					# Alors on met a True le master pour signifier que c'est complet
+					prefSpe[i]=(prefSpe[i][0],True);
+				# On met aussi l'etudiant à True pour signifier qu'il a un master
+				prefEtu[int(master_wish)]=(prefEtu[int(master_wish)][0], True); 
+				# print(prefSpe[int(master_wish)][1]);
+			else : # Sinon sa veut dire que le master est complet
+				# On va donc appeler la fonction result afin de voir si le master prefere le nouvel etudiant
+				result, prefEtu = prefer_cote_parcours(result, res, prefSpe[i], prefEtu);
+		i=i+1;
+	# print(result);
+	return result;
+
 if __name__ == '__main__':
-	prefSpe = openPref("TestPrefSpe.txt", 2);
+	prefSpe = openPref(sys.argv[2], 2);
 	# print(prefSpe);
-	prefEtu = openPref("TestPrefEtu.txt", 1)[0];
+	prefEtu = openPref(sys.argv[1], 1)[0];
 	# print(prefEtu);
 	resultat=gale_shapley_impl(prefSpe, prefEtu);
-	print(resultat);
+	print(sorted(resultat, key=itemgetter(1)));
+	#prefSpe = openPref(sys.argv[2], 2);
+	#prefEtu = openPref(sys.argv[1], 1)[0];
+	#resultat1=gale_shapley_parcours(prefSpe,prefEtu);
+	#print(resultat1);
 
 
